0bfada09dd2266b6eae96cd7ba97fc2c896f9dea
shprot05 <141780757+shprot05@users.noreply.github.com>
2025-08-21 13:46:30 +0200

    edit queries

--- main.py
+++ main.py
@@ -5,16 +5,16 @@ from queries import SqlExecuter
 def main():
     room_loader = RoomLoader()
     student_loader = StudentLoader()
-    room_loader.load_data("C:\\Users\\37529\\Downloads\\rooms.json")
-    student_loader.load_data("C:\\Users\\37529\\Downloads\\students.json")
+    #room_loader.load_data("C:\\Users\\37529\\Downloads\\rooms.json")
+    #student_loader.load_data("C:\\Users\\37529\\Downloads\\students.json")
 
     executor = SqlExecuter()
 
-    executor.students_count_in_rooms()
-    #executor.rooms_with_min_age()
+    #executor.students_count_in_rooms()
+    executor.rooms_with_min_age()
     #executor.rooms_with_max_age_diff()
-    #executor.m_and_f_rooms()
-
+    #executor.m_rooms()
+    #executor.f_rooms()
 
 if __name__ == "__main__":
     main()
