64415b8c12dd682eea0e71d033d8b2e61b53f38c
shprot05 <141780757+shprot05@users.noreply.github.com>
2025-08-21 13:46:42 +0200

    edit queries

--- queries.py
+++ queries.py
@@ -10,26 +10,36 @@ queries = {
         group by rooms.name
         order by count """,
     2: """SELECT 
-            rooms.id,
+            rooms.name,
             AVG(EXTRACT(YEAR FROM AGE(CURRENT_DATE, students.birthday))) AS avg_age
         FROM rooms 
         JOIN students ON rooms.id = students.room
-        GROUP BY rooms.id
+        GROUP BY rooms.name
         ORDER BY avg_age
         LIMIT 5 """,
-    3: """select room, 
+    3: """SELECT
+            rooms.name,
             MAX(EXTRACT(YEAR FROM AGE(CURRENT_DATE, students.birthday))) -
-            MIN(EXTRACT(YEAR FROM AGE(CURRENT_DATE, students.birthday))) as difference from students
-        Group by room
-        order by difference
-        limit 5 """,
-    4: """SELECT
-            rooms.id,
-            rooms.name
+            MIN(EXTRACT(YEAR FROM AGE(CURRENT_DATE, students.birthday))) AS age_difference
         FROM rooms
         INNER JOIN students on rooms.id = students.room
         GROUP BY rooms.id, rooms.name
-        HAVING COUNT(DISTINCT students.sex) > 1""",
+        ORDER BY age_difference
+        LIMIT 5 """,
+    4: """SELECT
+            rooms.name
+        FROM rooms
+        INNER JOIN students ON rooms.id = students.room
+        GROUP BY rooms.id, rooms.name
+        HAVING COUNT(DISTINCT students.sex) = 1
+        AND MAX(students.sex) = 'M'""",
+    5: """SELECT
+            rooms.name
+        FROM rooms
+        INNER JOIN students ON rooms.id = students.room
+        GROUP BY rooms.id, rooms.name
+        HAVING COUNT(DISTINCT students.sex) = 1
+        AND MAX(students.sex) = 'F'"""
 }
 
 
@@ -43,22 +53,28 @@ class SqlExecuter:
         self.cursor.execute(self.queries[1])
         result1 = self.cursor.fetchall()
         for room, count in result1:
-            print(f"{room}  {count}")
+            print(f"{room}  {count:2f}")
 
     def rooms_with_min_age(self):
         self.cursor.execute(self.queries[2])
         result2 = self.cursor.fetchall()
-        for room_id, avg in result2:
-            print(f"{room_id}  {avg}")
+        for room_name, avg in result2:
+            print(f"{room_name}  {avg:.2f}")
 
     def rooms_with_max_age_diff(self):
         self.cursor.execute(self.queries[3])
         result3 = self.cursor.fetchall()
         for room, diff in result3:
-            print(f"{room}  {diff}")
+            print(f"  {room}  {diff}")
 
-    def m_and_f_rooms(self):
+    def m_rooms(self):
         self.cursor.execute(self.queries[4])
         result4 = self.cursor.fetchall()
-        for room_id, room in result4:
-            print(f"{room_id}  {room} ")
+        for  room_name in result4:
+            print(f" {room_name} ")
+
+    def f_rooms(self):
+        self.cursor.execute(self.queries[5])
+        result4 = self.cursor.fetchall()
+        for  room_name in result4:
+            print(f" {room_name} ")
